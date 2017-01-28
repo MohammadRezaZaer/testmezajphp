e6b5c0a75604eba6ddf55fb6fa939390535ecd4c
mrzzrm <mrzzrm27@gmail.com>
2017-01-28 19:30:55 +0000

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,6 +4,11 @@ echo "androidiiiiiii";
 ob_start();
 define('API_KEY','329586540:AAEaZ-91maCKl87zFX9r-PlGs-vIkaIfEUA');
 
+$data = "https://api.telegram.org/bot329586540:AAEaZ-91maCKl87zFX9r-PlGs-vIkaIfEUA/sendmessage?chat_id=94036610&text=salam";
+
+$response = file_get_contents($data);
+
+
 function makeHTTPRequest($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
     $ch = curl_init();
