83e01fb5dccfde63330e3015050fd6f22950c7b2
mrzzrm <mrzzrm27@gmail.com>
2017-01-28 20:11:00 +0000

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$update = json_decode(file_get_contents('php://input'));
+$update = file_get_contents('php://input');
 
 $data = "https://api.telegram.org/bot329586540:AAEaZ-91maCKl87zFX9r-PlGs-vIkaIfEUA/sendmessage?chat_id=94036610&text=salam".$update;
 $response = file_get_contents($data);
