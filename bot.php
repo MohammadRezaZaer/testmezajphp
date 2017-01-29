5f5f8decbeb05665347a32c7dc10de6697ffae12
mrzzrm <mrzzrm27@gmail.com>
2017-01-29 23:43:39 +0000

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,19 +1,53 @@
 <?php
 
+define('API_KEY','329586540:AAEaZ-91maCKl87zFX9r-PlGs-vIkaIfEUA');
+
+
 $update = json_decode(file_get_contents('php://input'));
 
-$data = "https://api.telegram.org/bot329586540:AAEaZ-91maCKl87zFX9r-PlGs-vIkaIfEUA/sendmessage?chat_id=94036610&text=salam".$update->chat->first_name;
-$response = file_get_contents($data);
 
+if(isset($update->callback_query)){
+    $callbackMessage = 'آپدیت شد';
+    var_dump(makeHTTPRequest('answerCallbackQuery',[
+        'callback_query_id'=>$update->callback_query->id,
+        'text'=>$callbackMessage
+    ]));
+    $chat_id = $update->callback_query->message->chat->id;
+    $message_id = $update->callback_query->message->message_id;
+    $tried = $update->callback_query->data+1;
+    var_dump(
+        makeHTTPRequest('editMessageText',[
+            'chat_id'=>$chat_id,
+            'message_id'=>$message_id,
+            'text'=>($tried)." امین تلاش \n زمان : \n".date('d M y -  h:i:s'),
+            'reply_markup'=>json_encode([
+                'inline_keyboard'=>[
+                    [
+                        ['text'=>"رفرش زمان",'callback_data'=>"$tried"]
+                    ]
+                ]
+            ])
+        ])
+    );
 
-$$postdata = http_build_query(
-    array(
-        'chat_id' => '94036610',
-        'text' => 'posol'
-    )
-);
+}else{
+    var_dump(makeHTTPRequest('sendMessage',[
+        'chat_id'=>$update->message->chat->id,
+        'text'=>"اولین تلاش \n زمان :\n ".date('d M y -  h:i:s'),
+        'reply_markup'=>json_encode([
+            'inline_keyboard'=>[
+                [
+                    ['text'=>"رفرش زمان",'callback_data'=>'1']
+                ]
+            ]
+        ])
+    ]));
+}
+function makeHTTPRequest($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+   $postdata = http_build_query($datas);
 
-$opts = array('http' =>
+	$opts = array('http' =>
     array(
         'method'  => 'POST',
         'header'  => 'Content-type: application/x-www-form-urlencoded',
@@ -23,6 +57,6 @@ $opts = array('http' =>
 
 $context  = stream_context_create($opts);
 
-$result = file_get_contents('https://api.telegram.org/bot329586540:AAEaZ-91maCKl87zFX9r-PlGs-vIkaIfEUA/sendmessage', false, $context);
+$result = file_get_contents($url, false, $context);
+}
 
-define('API_KEY','329586540:AAEaZ-91maCKl87zFX9r-PlGs-vIkaIfEUA');
